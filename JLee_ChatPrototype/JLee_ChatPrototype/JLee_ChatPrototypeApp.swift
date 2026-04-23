55b01ded51f6bd49fbaed883f94e520e6ba682ba
Juliana Lee <julianalee295@gmail.com>
2026-04-23 09:00:26 +0900

    Create Xcode ChatPrototype Project

---
+++ JLee_ChatPrototype/JLee_ChatPrototype/JLee_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JLee_ChatPrototypeApp.swift
+//  JLee_ChatPrototype
+//
+//  Created by Juliana Lee on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct JLee_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
